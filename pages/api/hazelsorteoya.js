19b0ebafe09f0ed0cd4e33def8146e1ebdbceca0
albertot_dev <atejero.dam@gmail.com>
2022-07-06 18:49:05 +0200

    fix twitter errors and exclude retweet

--- pages/api/hazelsorteoya.js
+++ pages/api/hazelsorteoya.js
@@ -10,7 +10,9 @@ export default async function handler(req, res) {
   if (source.startsWith("#")) {
     const { data, includes } = await (
       await fetch(
-        `https://api.twitter.com/2/tweets/search/recent?tweet.fields=author_id&user.fields=&query=${source}&expansions=author_id`,
+        `https://api.twitter.com/2/tweets/search/recent?tweet.fields=author_id&user.fields=&query=${encodeURIComponent(
+          source
+        )}%20-is:retweet&expansions=author_id`,
         {
           headers: {
             Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
@@ -18,10 +20,16 @@ export default async function handler(req, res) {
         }
       )
     ).json();
-    result.attendees = data.map((tweet) => {
-      const user = includes.users.find((user) => user.id === tweet.author_id);
-      return `${user.name}(@${user.username}) | ${tweet.text}`;
-    });
+    if (data) {
+      return res.json({
+        message: "No se encontraron resultados",
+      });
+    }
+    result.attendees =
+      data?.map((tweet) => {
+        const user = includes.users.find((user) => user.id === tweet.author_id);
+        return `${user.name}(@${user.username}) | ${tweet.text}`;
+      }) || [];
   }
 
   if (source.indexOf("saraos.tech") > -1) {
